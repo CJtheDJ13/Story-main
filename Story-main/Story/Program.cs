1f4435082fb5a296217792658194959d24c8d7a9
CJtheDJ <carljohanedstrom@gmail.com>
2025-09-03 19:30:38 +0200

    StoryTest

---
+++ Story-main/Story/Program.cs
@@ -0,0 +1,23 @@
+﻿// bool isAvlive = 3 > 16;
+// bool isAvlive = 3 < 16;
+// bool isAvlive = 3 == 16;
+// bool isAvlive = 3 + 13 != 16;
+// bool isAvlive = 3 + 13 != 16 && 3 > 3;
+// Console.WriteLine(isAvlive);
+
+string choice = Console.ReadLine().ToLower();
+
+Console.WriteLine("ZOMBIE APOCALYPSE");
+Thread.Sleep(2500);
+Console.Clear();
+Console.WriteLine("Du vaknar upp i en hög av bråte, yr och nyvaken.");
+Thread.Sleep(250);
+Console.WriteLine("Med suddig syn ser du en helikopter en bit bort med din kanrat som vinkar dit dig.");
+Thread.Sleep(250);
+Console.WriteLine("Du måste ta dig dit på något sätt innan du blir uppäten av massorna av zombies som bara växer och växer.");
+Thread.Sleep(250);
+Console.WriteLine("Du ser dig omkring, till vänster ser du ett få antal zombies som inte verkar röra på sig.");
+Thread.Sleep(250);
+Console.WriteLine("Åt höger finns det ett litet skjul som");
+
+Console.ReadLine();
